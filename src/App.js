b387c1c13a2e4c26bae5d064948ba8c3864ce4bd
truongnv <truongnguyenvan0590@gmail.com>
2023-05-19 15:35:53 +0700

    import name and default name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import {FirstComponent} from './lessons/a_1_first_component/first_component'
 
 export default function Gallery() {
   return (
-      FirstComponent()
+      <FirstComponent/>
   );
 }
