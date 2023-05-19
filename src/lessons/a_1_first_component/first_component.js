d51a1e60c0466d502081dff837e3cf1e1a24d8fc
truongnv <truongnguyenvan0590@gmail.com>
2023-05-19 15:08:48 +0700

    first component

---
+++ src/lessons/a_1_first_component/first_component.js
@@ -0,0 +1,19 @@
+function Profile() {
+    // add markup
+    // this syn
+    return (
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStImZ_BSBPCeBGe6RIonkMw_D5sHB8xg0hVVDtoOE&s"
+            alt="TruongNV test"
+        />
+    );
+}
+
+export function FirstComponent() {
+    return (
+        <section>
+            <h1>Weibo never die</h1>
+            <Profile/>
+        </section>
+    );
+}
